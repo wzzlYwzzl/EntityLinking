5dea7f589e8f576ccf6d9960b13c183ed1f1914c
wzzlYwzzl <zjw123bit@126.com>
2019-08-29 17:00:06 +0800

    修改hits逻辑

--- entitylinking/graph/hits.py
+++ entitylinking/graph/hits.py
@@ -11,21 +11,21 @@ def hits_analyze(graph, iter):
             predecessors = graph.predecessors(node)
             for node_tmp in predecessors:
                 tmp += node_tmp.hub_weight
-            node.unnormarized_authority_weight = tmp * node.authority_weight_for_calculation
+            node.authority_weight += tmp * node.authority_weight_for_calculation
 
             tmp = 0.0
             successors = graph.successors(node)
             for node_tmp in successors:
                 tmp += node_tmp.authority_weight
-            node.unnormarized_hub_weight = tmp * node.hub_weight_for_calculation
+            node.hub_weight += tmp * node.hub_weight_for_calculation
 
         # normalize the weight
-        sum_authority = 0.0
-        sum_hub = 0.0
-        for node in graph.nodes:
-            sum_authority += node.authority_weight
-            sum_hub += node.hub_weight
+    sum_authority = 0.0
+    sum_hub = 0.0
+    for node in graph.nodes:
+        sum_authority += node.authority_weight
+        sum_hub += node.hub_weight
 
-        for node in graph.nodes:
-            node.authority_weight += node.unnormarized_authority_weight / sum_authority
-            node.hub_weight += node.unnormarized_hub_weight / sum_hub
+    for node in graph.nodes:
+        node.authority_weight = node.authority_weight / sum_authority
+        node.hub_weight = node.hub_weight / sum_hub
