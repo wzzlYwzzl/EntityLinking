87ef63614d5d9b1098eface186a852b06aa1d5ec
wzzlYwzzl <zjw123bit@126.com>
2019-08-29 11:39:04 +0800

    修改实体识别的逻辑

--- entitylinking/ner/dict_ner.py
+++ entitylinking/ner/dict_ner.py
@@ -1,6 +1,6 @@
 import threading
 
-from entitylinking.jieba import Tokenizer
+import entitylinking.jieba as jieba
 from ..base.mention import Mention
 
 from ..config.app_config import AppConfig
@@ -27,16 +27,28 @@ class DictNER:
     def __init__(self, dic_file):
         """dic_file文件的词典是由tab分割
         """
-        self.tokenizer = Tokenizer(dictionary=dic_file, split='\t')
+        self._entity_dict = {}
+        self._load_dic_file(dic_file)
 
     def get_mentions(self, text):
         """从text中识别词典中的词
         """
-        words = self.tokenizer.cut_from_dict(text)
+        words = jieba.tokenize(text, cut_all=False)
 
         mentions = []
         for word, start, end in words:
-            mention = Mention(start, end - start, word)
-            mentions.append(mention)
+            if word in self._entity_dict:
+                mention = Mention(start, end - start, word)
+                mentions.append(mention)
 
         return mentions
+
+    def _load_dic_file(self, dic_file):
+        """加载词典文件
+        """
+        with open(dic_file, mode='r', encoding='utf-8') as f:
+            for line in f:
+                fields = line.split('\t')
+                if len(fields) >= 1:
+                    key = fields[0].strip()
+                    self._entity_dict[key] = fields[1:]
