d06a9c102993833fb91523f1d317faf92cc95c80
wzzlYwzzl <zjw123bit@126.com>
2019-09-03 19:28:55 +0800

    update dict

--- entitylinking/bin/handle_synonym.py
+++ entitylinking/bin/handle_synonym.py
@@ -1,5 +1,5 @@
 # 原始要加工的文件
-file_synonym = "/Users/caoxiaojie/pythonCode/EntityLinking/tools/elasticsearch/elasticsearch-jieba-plugin-7.3.1/dic/synonym.txt"
+file_synonym = "/Users/caoxiaojie/pythonCode/EntityLinking/tools/elasticsearch/elasticsearch-jieba-plugin-7.3.1/dic/synonyms.txt"
 
 # 要得到的文件
 file_out = "/Users/caoxiaojie/pythonCode/EntityLinking/tools/elasticsearch/elasticsearch-jieba-plugin-7.3.1/dic/out.txt"
@@ -18,5 +18,18 @@ def handle_synonym(file_in, file_out):
                             f_out.write(words)
                             f_out.write('\n')
 
+
+def create_synonym_dict(file_in, file_out):
+    word_set = set()
+    with open(file_in, mode='r', encoding='utf-8') as f:
+        with open(file_out, mode='w+', encoding='utf-8') as f_out:
+            for line in f:
+                fields = line.split(',')
+                for field in fields:
+                    word = field.strip()
+                    if word and word not in word_set:
+                        word_set.add(word)
+                        f_out.write('{} 10\n'.format(word))
+
 if __name__ == '__main__':
-    handle_synonym(file_synonym, file_out)
+    create_synonym_dict(file_synonym, file_out)
