a7195ab50d99d5e141168a47b6789c9eabebb6ce
wzzlYwzzl <zjw123bit@126.com>
2019-09-06 16:54:16 +0800

    添加mention2id代码

--- entitylinking/bin/cn_pedia_ids.py
+++ entitylinking/bin/cn_pedia_ids.py
@@ -17,10 +17,41 @@ def id_cn_dbpedia(cn_pedia_file, id_file):
                         subject_id += 1
                         id_dict[subject] = subject_id
                         f_out.write('{}\t{}\n'.format(subject, subject_id))
-                        
+
+
+def load_id_file(id_file):
+    """加载id文件
+    """
+    id_dict = {}
+    with open(id_file, mode='r', encoding='utf-8') as f:
+        for line in f:
+            fields = line.split('\t')
+            id_dict[fields[0]] = fields[1]
+
+    return id_dict
+
+
+def mention2id(mention_file, id_file, mention_out):
+    """将menton2e.txt文件中的内容也转换为id
+    """
+    id_dict = load_id_file(id_file)
+    with open(mention_file, mode='r', encoding='utf-8') as f:
+        with open(mention_out, mode='w+', encoding='utf-8') as f_out:
+            for line in f:
+                fields = line.split('\t')
+                mention = fields[0].strip()
+                entity = fields[1].strip()
+                if entity in id_dict:
+                    f_out.write('{}\t{}\n'.format(mention, id_dict[entity]))
+                else:
+                    f_out.write('{}\tNULL\n'.format(mention))
+
 
 cndbpedia_file = "/Users/caoxiaojie/pythonCode/EntityLinking/data/cn-pedia/test.txt"
 id_file = "/Users/caoxiaojie/pythonCode/EntityLinking/data/cn-pedia/id.txt"
+m2e = "/Users/caoxiaojie/pythonCode/EntityLinking/data/cn-pedia/m2e.txt"
+m2id = "/Users/caoxiaojie/pythonCode/EntityLinking/data/cn-pedia/m2id.txt"
 
 if __name__ == '__main__':
-    id_cn_dbpedia(cndbpedia_file, id_file)
+    #id_cn_dbpedia(cndbpedia_file, id_file)
+    mention2id(m2e, id_file, m2id)
